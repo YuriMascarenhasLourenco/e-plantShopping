e8042ae41ca76214bbab8dcbd97f3a444f2ccc16
YuriMascarenhasLourenco <lourenco99yuri@gmail.com>
2025-11-06 16:50:27 -0300

    h1 and cart icon fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -294,12 +294,14 @@ const calculateTotalQuantity = () => {
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> 
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            
                             <h1 className='cart'>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                                 <rect width="156" height="156" fill="none"></rect>
                                 <circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle>
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
                                 </svg>
+                                {CartItems.length>0 ? <span className="cart-badge">{CartItems.length}</span> : ''}
                             </h1>
                         </a>
                     </div>
@@ -313,7 +315,7 @@ const calculateTotalQuantity = () => {
               {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                 <div key={index}> {/* Unique key for each category div */}
                     <h1>
-                        <div>{category.category}</div> {/* Display the category name */}
+                        <div className='product-title'>{category.category}</div> {/* Display the category name */}
                     </h1>
                     <div className="product-list"> {/* Container for the list of plant cards */}
                     {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
@@ -327,14 +329,16 @@ const calculateTotalQuantity = () => {
                         {/* Display other plant details like description and cost */}
                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
                         <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
-                        <button
+                        {!addedToCart[plant.name] ? <button
                             className="product-button"
                             onClick={() => handleAddToCart(plant)
                             } // Handle adding plant to cart
                             disabled={!!addedToCart[plant.name]}
                         >
                             Add to Cart
-                        </button>
+                        </button>:
+                        <button className="product-button" disabled>Added to Cart</button>
+                        }
                         </div>
                     ))}
     </div>
